6f21b22fd6929b0b2e79aec38fab3a6499e41c82
Jason Lewis <jm.lewis@gmail.com>
2022-05-20 17:42:54 +1000

    Remove shared dynamic form API.

--- libs/users/feature-manage/src/lib/users-feature-manage.module.ts
+++ libs/users/feature-manage/src/lib/users-feature-manage.module.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedApiStatusUiModule } from '@app/shared/api-status/ui';
-import { SharedDynamicformApiModule } from '@app/shared/dynamicform/api';
 import { UsersDomainModule } from '@app/users/domain';
 import { UsersUiManageModule } from '@app/users/ui-manage';
 
 import { UsersFeatureManageRoutingModule } from './users-feature-manage-routing.module';
 import { UsersManageProfilesComponent } from './users-manage-profiles/users-manage-profiles.component';
 
-const sharedModules = [SharedDynamicformApiModule, SharedApiStatusUiModule];
+const sharedModules = [SharedApiStatusUiModule];
 const domainModules = [UsersDomainModule, UsersUiManageModule];
 
 @NgModule({
