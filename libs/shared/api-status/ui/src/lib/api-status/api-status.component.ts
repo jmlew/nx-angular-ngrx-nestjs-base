cc26f11f54f4d836ba9e6c178121fd65f7779206
Jason Lewis <jm.lewis@gmail.com>
2022-05-26 21:39:56 +1000

    Update API Status container to optionally display each status type if passed as templates.

--- libs/shared/api-status/ui/src/lib/api-status/api-status.component.ts
+++ libs/shared/api-status/ui/src/lib/api-status/api-status.component.ts
@@ -1,5 +1,22 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ContentChild,
+  Input,
+  TemplateRef,
+} from '@angular/core';
 import * as fromApiStatus from '@app/shared/api-status/util';
+
+/*
+  Template for displaying content based on API status.
+  Consumer can provide a template for content to display conditionally for each status or
+  just add content to be rendered via the ng-content directive if the default failed and
+  pending content can be displayed.
+
+  <ng-template #success>success content here</ng-template
+  <ng-template #failed>failed content here</ng-template
+  <ng-template #pending>pending content here</ng-template
+*/
 
 @Component({
   selector: 'app-api-status',
@@ -9,6 +26,9 @@ import * as fromApiStatus from '@app/shared/api-status/util';
 })
 export class ApiStatusComponent {
   readonly ApiStatus = fromApiStatus.ApiStatus;
+  @ContentChild('success') success: TemplateRef<unknown>;
+  @ContentChild('failed') failed: TemplateRef<unknown>;
+  @ContentChild('pending') pending: TemplateRef<unknown>;
 
   @Input() requestState: fromApiStatus.ApiRequestState;
 }
