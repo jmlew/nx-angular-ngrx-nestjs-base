9819e77bc4bddcfe1fc587fceb3115355e3d384d
Jason Lewis <jm.lewis@gmail.com>
2022-06-01 10:28:38 +1000

    Create custom route selectors to access all params and query params.

--- libs/shared/navigation/domain/src/lib/+state/router/router.selectors.ts
+++ libs/shared/navigation/domain/src/lib/+state/router/router.selectors.ts
@@ -1,4 +1,9 @@
-import { RouterReducerState, getSelectors } from '@ngrx/router-store';
+import { ActivatedRouteSnapshot, Params } from '@angular/router';
+import {
+  RouterReducerState,
+  SerializedRouterStateSnapshot,
+  getSelectors,
+} from '@ngrx/router-store';
 import { createFeatureSelector } from '@ngrx/store';
 
 import { ROUTER_FEATURE_KEY } from '.';
@@ -15,3 +20,31 @@ export const {
   selectRouteData,
   selectUrl,
 } = getSelectors(selectRouter);
+
+export function selectAllRouteParams(snapshot: SerializedRouterStateSnapshot): Params {
+  let route: ActivatedRouteSnapshot = snapshot.root;
+  const params: Map<string, Params> = new Map(
+    Object.keys(route.params).map((key: string) => [key, route.params[key]])
+  );
+  while (route.firstChild) {
+    route = route.firstChild;
+    Object.keys(route.params).forEach((key: string) =>
+      params.set(key, route.params[key])
+    );
+  }
+  return params;
+}
+
+export function selectAllQueryParams(snapshot: SerializedRouterStateSnapshot): Params {
+  let route: ActivatedRouteSnapshot = snapshot.root;
+  const params: Map<string, Params> = new Map(
+    Object.keys(route.queryParams).map((key) => [key, route.queryParams[key]])
+  );
+  while (route.firstChild) {
+    route = route.firstChild;
+    Object.keys(route.queryParams).forEach((key) =>
+      params.set(key, route.queryParams[key])
+    );
+  }
+  return params;
+}
